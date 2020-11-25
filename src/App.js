aa11aa9b467771302cc32fa4c3ded339d2a7dc39
Paden <paden@within.co.kr>
2020-11-25 14:33:03 +0900

    Using useEffect And axios api module

--- src/App.js
+++ src/App.js
@@ -1,74 +1,102 @@
-import './App.css'
-import React, { useState } from 'react'
-import { Button, Jumbotron, Nav, Navbar, NavDropdown } from 'react-bootstrap'
-import Data from './data'
-import Product from './components/Product'
-import { Route } from 'react-router-dom'
+import "./App.css";
+import React, { useState } from "react";
+import { Button, Jumbotron, Nav, Navbar, NavDropdown } from "react-bootstrap";
+import Data from "./data";
+import Product from "./components/Product";
+import Detail from "./components/Detail";
+import axios from "axios";
 
-function App () {
+import { Link, Route, Switch } from "react-router-dom";
 
-  let [shoes, shoes변경] = useState(Data)
+function App() {
+  let [shoes, shoes변경] = useState(Data);
+
+  let shoes추가 = (data) => {
+    let newArray = [...shoes];
+    data.map((item) => {
+      item.img = "https://codingapple1.github.io/shop/shoes1.jpg";
+      newArray.push(item);
+    });
+    shoes변경(newArray);
+  };
 
   return (
     <div className="App">
       <Navbar bg="light" expand="lg">
         <Navbar.Brand href="#home">Shoe mall</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav"/>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
+            <Nav.Link as={Link} to="/">
+              Home
+            </Nav.Link>
+            <Nav.Link as={Link} to="/detail">
+              Detail
+            </Nav.Link>
             <NavDropdown title="Dropdown" id="basic-nav-dropdown">
               <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Another action
+              </NavDropdown.Item>
               <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider/>
-              <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Separated link
+              </NavDropdown.Item>
             </NavDropdown>
           </Nav>
         </Navbar.Collapse>
       </Navbar>
 
-      <Route exact path="/">
-        <Jumbotron className="background">
-          <h1>20% Season OFF</h1>
-          <p>
-            This is a simple hero unit, a simple jumbotron-style component for calling
-            extra attention to featured content or information.
-          </p>
-          <p>
-            <Button variant="primary">Learn more</Button>
-          </p>
-        </Jumbotron>
+      {/* Switch : 라우터중에 가장 먼저 매칭되는거 선택해주세요.*/}
+      <Switch>
+        <Route exact path="/">
+          <Jumbotron className="background">
+            <h1>20% Season OFF</h1>
+            <p>
+              This is a simple hero unit, a simple jumbotron-style component for
+              calling extra attention to featured content or information.
+            </p>
+            <p>
+              <Button variant="primary">Learn more</Button>
+            </p>
+          </Jumbotron>
 
-        <div className="container">
-          <div className="row">
-            {
-              shoes.map((item) => {
-                return (<Product item={item} key={item.id}></Product>)
-              })
-            }
+          <div className="container">
+            <div className="row">
+              {shoes.map((item) => {
+                return <Product item={item} key={item.id}></Product>;
+              })}
+            </div>
+            <button
+              className="btn btn-primary"
+              onClick={() => {
+                axios
+                  .get("https://codingapple1.github.io/shop/data2.json")
+                  .then((result) => {
+                    console.log("호출 성공 :", result.data);
+                    shoes추가(result.data);
+                  })
+                  .catch(() => {
+                    console.log("호출 실패");
+                  });
+              }}
+            >
+              더보기
+            </button>
           </div>
-        </div>
-      </Route>
+        </Route>
 
-      <Route path="/detail">
-        <div className="container">
-          <div className="row">
-            <div className="col-md-6">
-              <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%"/>
-            </div>
-            <div className="col-md-6 mt-4">
-              <h4 className="pt-5">상품명</h4>
-              <p>상품설명</p>
-              <p>120000원</p>
-              <button className="btn btn-danger">주문하기</button>
-            </div>
-          </div>
-        </div>
-      </Route>
+        <Route path="/detail/:id">
+          <Detail shoes={shoes} />
+        </Route>
+
+        <Route path="/:id">
+          <div>아무거나</div>
+        </Route>
+      </Switch>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Detail.js
@@ -0,0 +1,111 @@
+import React, { useEffect, useState } from "react";
+import { useHistory, useParams } from "react-router-dom";
+import styled from "styled-components";
+import "../css/Detail.scss";
+
+// Life Cycle Hook 옛날에는 이렇게 component 생명 주기 관리.
+// 생성, 재랜더링, 삭제
+class Detail2 extends React.Component {
+  componentDidMount() {
+    //Detail2 컴포넌트가 Mount 되고나서 실행할 코드
+  }
+
+  componentWillUnmount() {
+    //Detail2 컴포넌트가 Unmount 되기전에 실행할 코드
+  }
+}
+
+let 박스 = styled.div`
+  padding: 20px;
+`;
+let 제목 = styled.h4`
+  font-size: 20px;
+  color: ${(props) => props.색상};
+`;
+
+function Detail(props) {
+  let [alert, alert변경] = useState(true);
+  let [입력값, 입력값변경] = useState("");
+
+  let changeValue = (e) => {
+    입력값변경(e.target.value);
+  };
+
+  let submitValue = () => {
+    console.log(입력값);
+  };
+
+  useEffect(() => {
+    console.log("created");
+    let 타이머 = setTimeout(() => {
+      // document.getElementById("my-alert").style.display="none";
+      console.log("alert 실행");
+      alert변경(false);
+    }, 2000);
+
+    return () => {
+      clearTimeout(타이머);
+      console.log("exit");
+    };
+  }, [alert]);
+
+  let { id } = useParams();
+  let history = useHistory();
+
+  let shoe = props.shoes.find((item) => item.id == id);
+
+  if (shoe == null) return <div>없는 상품입니다.</div>;
+
+  return (
+    <div className="container">
+      <박스>
+        <제목 색상="blue">Detail</제목>
+      </박스>
+
+      <input onChange={changeValue} />
+      <button onClick={submitValue}>입력</button>
+
+      {alert === true ? (
+        <div id="my-alert" className="my-alert">
+          <p>재고가 얼마 남지 않았습니다.</p>
+        </div>
+      ) : null}
+
+      {/*<div name="my-alert" className="my-alert-red">*/}
+      {/*  <p>재고가 얼마 남지 않았습니다.</p>*/}
+      {/*</div>*/}
+      {/*<div name="my-alert" className="my-alert-black">*/}
+      {/*  <p>재고가 얼마 남지 않았습니다.</p>*/}
+      {/*</div>*/}
+      <div className="row">
+        <div className="col-md-6">
+          <img src={shoe.img} width="100%" />
+        </div>
+        <div className="col-md-6 mt-4">
+          <h4 className="pt-5">{shoe.title}</h4>
+          <p>{shoe.content}</p>
+          <p>{shoe.price}</p>
+          <button className="btn btn-danger">주문하기</button>
+          <button
+            className="btn btn-danger"
+            onClick={() => {
+              history.goBack();
+            }}
+          >
+            뒤로가기
+          </button>
+          <button
+            className="btn btn-danger"
+            onClick={() => {
+              history.push("/");
+            }}
+          >
+            메인페이지로 이동
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Detail;
